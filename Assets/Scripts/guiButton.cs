91884bfca24ab54f381a7ef82825555fdc0c164d
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2016-05-06 17:12:01 -0400

    Level1 GUIButtons - Working - Can navigate to main menu and exit - Prefabs ready for anything

--- Assets/Scripts/guiButton.cs
+++ Assets/Scripts/guiButton.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 using UnityEngine.UI;
 
-public class guiButton : MonoBehaviour {
 
+
+public class guiButton : MonoBehaviour {
+	
 	public void Click() {
 		print ("MEOOOOW");
-		if (this.GetComponent<Text> ().text == "Exit") {
-			print (this.GetComponent<Text> ().text);
+		print (this.GetComponentInChildren<Text> ().text);
+		if (this.GetComponentInChildren<Text> ().text == "Exit") {
+			print(this.GetComponentInChildren<Text> ().text);
+			Application.Quit ();
+		} else if (this.GetComponentInChildren<Text> ().text == "Menu") {
+			print("THIS NOT WORKING");
+			Application.LoadLevel("Main");
 		}
 
 	}
