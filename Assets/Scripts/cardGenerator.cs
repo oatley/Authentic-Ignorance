d919d4a972a6fcc932fdf749b04fef80bac41870
Andrew Oatley-Willis <oatleywillisa@gmail.com>
2016-05-07 01:02:37 -0400

    Generating random card - Can now generate the data for all cards - Does not assign the data to cards

--- Assets/Scripts/cardGenerator.cs
+++ Assets/Scripts/cardGenerator.cs
@@ -9,6 +9,13 @@ public class cardGenerator : MonoBehaviour {
 	List<string> good = new List<string>();
 	List<string> bad = new List<string>();
 	List<string> neutral = new List<string>();
+
+	// Card values
+	public int[] goodvalues = {-4,-3,1,2,3};
+	public int[] badvalues = {-2,-1,3,4,4};
+	public int[] neutralvalues = {-3,-2,2,3,3};
+
+
 	
 	// Use this for initialization
 	void Start () {
@@ -36,10 +43,26 @@ public class cardGenerator : MonoBehaviour {
 		input_stream.Close( );
 	}
 
-	void generateCard() {
-		int alignment = Random.Range (1, 3);
-		int some1 = Random.Range (1, 3);
-		int dude = Random.Range (1, 3);
+	public void generateCard() {
+		int cardalignment = new int();
+		int cardscore = new int ();
+		cardalignment = Random.Range (1, 4);
+		cardscore = Random.Range (0, 4);
+
+
+		if (cardalignment == 1) { // good
+			string card = good [Random.Range (0, good.Count)]; // card name
+			print (card + " good " + goodvalues [cardscore].ToString ()); // string = name alignment score
+		} else if (cardalignment == 2) { // bad
+			string card = bad [Random.Range (0, bad.Count)]; // card name
+			print (card + " bad " + badvalues [cardscore].ToString ()); // string = name alignment score
+		} else if (cardalignment == 3) { // neutral
+			string card = neutral [Random.Range (0, neutral.Count)]; // card name
+			print (card + " neutral " + neutralvalues [cardscore].ToString ()); // string = name alignment score
+		} else {
+			print("meow good 20");
+		}
+
 	}
 
 
